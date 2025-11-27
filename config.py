19c9f665d4ece6411b2b07691e25ca8055115f57
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-11-27 14:25:43 +0530

    Add API_KEY to config.py

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ load_dotenv()
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
+API_KEY = "0122a3195bd84ac3a"
+
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
